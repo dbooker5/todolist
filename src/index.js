090d89cb471dd14d6959d72719691c8e194562c8
dbooker5 <bookerotieno21@gmail.com>
2025-09-02 13:11:27 +0300

    Add Header component for improved layout structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import {render} from 'react-dom/client';
-import {App} from './App';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
 
