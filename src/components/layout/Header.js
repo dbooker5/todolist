9ec4ec31e835443e8b0d41bfed68bc6364332a98
dbooker5 <bookerotieno21@gmail.com>
2025-09-18 19:02:09 +0300

    Updated logo path and added data-testid and class names to settings items.

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,13 +7,12 @@ export const Header = () => {
     return <header className="header" data-testid= "header">
         <nav>
             <div className="logo">
-                <img src="/image/logo.png" alt="Todolist"/>
+                <img src="/images/logo.png" alt="Todolist"/>
             </div>
             <div className="settings">
                 <ul>
-                    <li>+</li>
-                    <li><FaPizzaSlice /></li>
-                    <li>Logout</li>
+                    <li data-testid="quick-add-task-action" className='settings__add'>+</li>
+                    <li data-testid="dark-mode-action" className='settings__darkmode'><FaPizzaSlice /></li>
                 </ul>
             </div>
         </nav>
